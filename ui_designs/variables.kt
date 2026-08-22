158bb23de406ea7bb0557a189a6e2bda12d49a21
carma212107-afk <carma212107@gmail.com>
2026-08-22 16:41:50 +0000

    variables file created with main colours and currentUI dimensions

---
+++ ui_designs/variables.kt
@@ -0,0 +1,26 @@
+object Colours {
+  val Accent: Color = Color(0xFF1A9E52)
+
+    // Light mode colors
+  val LightModeBackground1: Color = Color(0xFFF5FFF8)
+  val LightModeBackground2: Color = Color(0xFFFFFFFF)
+  val LightModePrimary: Color = Color(0xFF0A5C2E)
+  val LightModeSecondary: Color = Color(0xFFB5DDC3)
+  val LightModeBorder: Color = Color(0xFFB5DDC3)
+  val LightModeText: Color = Color(0xFF0A5C2E)
+  val LightModeInputText: Color = Color(0xFF1A9E52)
+
+    // Dark mode colors
+  val DarkModeBackground: Color = Color(0xFF0A5C2E)
+  val DarkModePrimary: Color = Color(0xFF2A7149)
+  val DarkModeSecondary: Color = Color(0xFFF5FFF8)
+  val DarkModeBorder: Color = Color(0xFFF5FFF8)
+  val DarkModeText: Color = Color(0xFFFFFFFF)
+  val DarkModeInputText: Color = Color(0xFFFFFFFF)
+}
+
+
+object Dimensions {
+  val currentUIX = 402.dp
+  val currentUIY = 874.dp
+}
