58dad0f5cbf0b69d6b540cf2225ace402cbd0fad
carma212107-afk <carma212107@gmail.com>
2026-08-25 15:45:48 +0000

    privatising ride components

--- ui_designs/ride_components.kt
+++ ui_designs/ride_components.kt
@@ -26,7 +26,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun RideCard(
+private fun RideCard(
 	startCity: String,
 	endCity: String,
 	date: String,
@@ -288,7 +288,7 @@ fun DiscussingRideBanner(ride: Ride, modifier: Modifier = Modifier) {
 
 // Seat icons and no_of_free_seats indicator
 @Composable
-fun NoOfFreeSeatsIndicator(modifier: Modifier = Modifier, noOfFreeSeats: Int = 3, noOfSeats: Int = 4) {
+private fun NoOfFreeSeatsIndicator(modifier: Modifier = Modifier, noOfFreeSeats: Int = 3, noOfSeats: Int = 4) {
 	Row(modifier = modifier.padding(horizontal = 2.dp), horizontalArrangement = Arrangement.spacedBy(2.dp)) {
 		for (i in 1..noOfSeats) {
             IndividualSeatIcon(available = i <= noOfFreeSeats)
@@ -296,7 +296,7 @@ fun NoOfFreeSeatsIndicator(modifier: Modifier = Modifier, noOfFreeSeats: Int = 3
 	}
 }
 @Composable
-fun IndividualSeatIcon(available: Boolean = false, modifier: Modifier = Modifier) {
+private fun IndividualSeatIcon(available: Boolean = false, modifier: Modifier = Modifier) {
 	Box(
 		modifier = modifier.size(10.dp).border(1.dp, Colours.LightModePrimary, RoundedCornerShape(3.dp))
 			.background(if (available) Colours.LightModeBackground1 else Colours.LightModePrimary, RoundedCornerShape(3.dp)),
